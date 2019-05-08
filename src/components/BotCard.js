d26af977fd728a2dd8fc0601316491ef1ecd466a
campbelllssoup <matjocampbell@gmail.com>
2019-05-08 10:39:36 -0400

    Checkpoint

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -1,11 +1,10 @@
 import React from "react";
 
 const BotCard = props => {
-  const { bot } = props;
 
   let botType;
 
-  switch (bot.bot_class) {
+  switch (props.bot_class) {
     case "Assault":
       botType = <i className="icon military" />;
       break;
@@ -23,34 +22,34 @@ const BotCard = props => {
     <div className="ui column">
       <div
         className="ui card"
-        key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        key={props.id}
+        onClick={() => { props.handleClick(props) }}
       >
         <div className="image">
-          <img alt="oh no!" src={bot.avatar_url} />
+          <img alt="oh no!" src={props.avatar_url} />
         </div>
         <div className="content">
           <div className="header">
-            {bot.name} {botType}
+            {props.name} {botType}
           </div>
 
           <div className="meta text-wrap">
-            <small>{bot.catchphrase}</small>
+            <small>{props.catchphrase}</small>
           </div>
         </div>
         <div className="extra content">
           <span>
             <i className="icon heartbeat" />
-            {bot.health}
+            {props.health}
           </span>
 
           <span>
             <i className="icon lightning" />
-            {bot.damage}
+            {props.damage}
           </span>
           <span>
             <i className="icon shield" />
-            {bot.armor}
+            {props.armor}
           </span>
         </div>
       </div>
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,44 @@
 import React from "react";
+import BotCollection from './BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+
+  constructor() {
+    super()
+    this.state = {
+      bots: [],
+      yourBots: []
+    }
+  }
+
+  addBot = (props) => {
+    if (this.state.yourBots.find( bot => { return bot.id === props.id }) === undefined ) {
+      let yourNewBots = [...this.state.yourBots, props]
+      this.setState({yourBots: yourNewBots})
+    }
+  }
+
+  removeBot = (props) => {
+    let yourNewBots = this.state.yourBots.filter( bot => {
+      return bot.id !== props.id
+    })
+    this.setState({yourBots: yourNewBots})
+  }
+
+  componentDidMount() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then(bots => {
+      this.setState({bots: bots})
+    })
+  }
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <BotCollection addBot={this.addBot} bots={this.state.bots}/>
+        <YourBotArmy removeBot={this.removeBot} yourBots={this.state.yourBots}/>
       </div>
     );
   }
