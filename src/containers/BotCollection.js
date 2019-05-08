fd36600d8bad523b9b20fc0ddeddded41a261a01
joshrs1199 <46760607+joshrs1199@users.noreply.github.com>
2019-05-08 11:17:51 -0400

    idk

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -5,10 +5,13 @@ class BotCollection extends React.Component {
   //your code here
 
   render(){
+		let filterSearch = this.props.bots.filter(bot => bot.name.toLowerCase().includes(this.props.currentSearch.toLowerCase()))
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
-    		  {/*...and here..*/}
+    		  {filterSearch.map((bot) => {
+						return <BotCard bot={bot} currentBotClick={this.props.currentBotClick}/>
+					})}
     		  Collection of all bots
     		</div>
   	  </div>
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -5,11 +5,14 @@ class YourBotArmy extends React.Component {
   //your bot army code here...
 
   render(){
+    let botTeam = this.props.team.map((bot) => {
+      return <BotCard bot={bot}/>
+    })
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {botTeam}
             Your Bot Army
           </div>
         </div>
